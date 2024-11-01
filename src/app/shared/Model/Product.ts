822c89d140bda84be4e2f227ef9c88bf28e5ac14
azizkhalsi <80319568+azizkhalsi@users.noreply.github.com>
2024-11-01 20:57:42 +0100

    angular-nginx

--- src/app/shared/Model/Product.ts
+++ src/app/shared/Model/Product.ts
@@ -1,8 +1,11 @@
+// src/app/shared/Model/Product.ts
 export class Product {
-    idProduit : any;
-    codeProduit : any;
-    libelleProduit : any;
-    prix : any;
-    dateCreation:any;
-    dateDerniereModification:any;
+  idProduit: number | null = null;  // Primary key, nullable when creating
+  codeProduit: string | null = null;  // Unique code for the product
+  libelleProduit: string | null = null;  // Product name or label
+  prix: number | null = null;  // Product price
+  dateCreation: Date | null = null;  // Creation date of the product
+  dateDerniereModification: Date | null = null;  // Last modification date
+  categorieProduitId: number | null = null;  // Foreign key for category
+  stockId: number | null = null;  // Foreign key for stock
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
 
-  url: 'http://localhost:8089/projet',
+  url: 'http://192.168.154.130:8089/projet',
 };
